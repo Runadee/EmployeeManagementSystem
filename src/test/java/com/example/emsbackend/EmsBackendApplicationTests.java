52638b44a4cfc522574e240db8752a22208e0256
Edanur <edanurgurgen94@gmail.com>
2024-04-17 13:47:21 +0300

    Initial commit: Added project files including .gitignore, Maven wrapper configuration, POM.xml, main application file, entity class, application properties, and a test class

---
+++ src/test/java/com/example/emsbackend/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
